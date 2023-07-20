9cb31e6acf846dae6b07d9c67e87caf5c39964fa
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2023-07-21 02:11:01 +0530

    added atomic components

---
+++ src/components/Responsive/components/BlurboxComp.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BlurboxComp = () => {
+	return (
+		<div className="w-[263px] h-9 px-4 py-2.5  rounded-2xl shadow-inner border border-black border-opacity-5 backdrop-blur-md justify-start items-start gap-2.5 inline-flex">
+			<div className="text-center flex flex-row">
+				<div className="text-blue-700 text-xs font-medium leading-none">
+					$BLUR{' '}
+				</div>
+				<div className="text-gray-700 text-xs font-medium leading-none">
+					{' '}
+					Lending taken to a different level
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default BlurboxComp;
--- src/components/Responsive/index.tsx
+++ src/components/Responsive/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import TextComponent from './components/TextComponent';
 import Box1 from './components/Box1';
+import Box3 from './components/Box3';
+import Box2 from './components/Box2';
+import BlurboxComp from './components/BlurboxComp';
 
 const LandingPage = () => {
 	return (
 		<div>
 			{/* <TextComponent /> */}
-			<Box1 />
+			{/* <Box1 /> */}
+			{/* <Box2 /> */}
+			{/* <Box3 /> */}
+			<BlurboxComp />
 		</div>
 	);
 };
