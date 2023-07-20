064d40df3ad51776fc3bdddd146b735f818175ba
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2023-07-21 04:10:44 +0530

    structuring initiated

--- src/components/Responsive/index.tsx
+++ src/components/Responsive/index.tsx
@@ -8,15 +8,30 @@ import TextComponent2 from './components/TextComponent2';
 import Box4 from './components/Box4';
 import BannerComp from './components/BannerComp';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Image from 'next/image';
+import BackgroundImage from '../../../public/static/img/group-1171274935.png';
 
 const LandingPage = () => {
 	return (
 		<div>
+			<div>
+				<Image
+					className="w-[120%] self-stretch absolute"
+					src={BackgroundImage}
+					alt="image"
+				/>
+			</div>
+			<Navbar />
+			<BlurboxComp />
 			{/* <TextComponent /> */}
-			{/* <Box1 /> */}
-			{/* <Box2 /> */}
-			{/* <Box3 /> */}
-			{/* <BlurboxComp /> */}
+			<div className="flex flex-row items-center justify-center relative top-[160px] gap-4">
+				<Box1 />
+				<div className="flex flex-col items-center justify-center relative">
+					<Box2 />
+					<Box3 />
+				</div>
+			</div>
 			{/* <TextComponent2 /> */}
 			<Box4 />
 			<BannerComp />
